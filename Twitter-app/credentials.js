d6b9e47024376bd43bc0b93d7bbafe11df8a52f1
HopeCheung <568038810@qq.com>
2019-04-15 02:42:56 -0400

    some big data project

---
+++ Twitter-app/credentials.js
@@ -0,0 +1,6 @@
+module.exports = {
+"consumer_key" : "R3iYOM47n2TD4uNNIafuO6pM3",
+"consumer_secret" : "WDbV7DAED63sdveK0rezpBnTvS7N08YcAUvM0Zx7sARuyAA0Aj" ,
+"access_token_key" : "1051887802391154689-rVPND8LnALxFyxQlBDi8IjoPid23aa" ,
+"access_token_secret" : "3YSP9XKIu5bixbW6LxkfnbKOWt2Cx4tlz7aYHSS5YMoyN",
+}
